0e8dd63f2884933b7840b5ffb0e9c7c3f5190234
raylu <lurayl@gmail.com>
2017-10-20 01:57:38 -0700

    green flame

--- sleeperbot_mumble.py
+++ sleeperbot_mumble.py
@@ -19,50 +19,15 @@ from pymumble.constants import PYMUMBLE_CLBK_TEXTMESSAGERECEIVED
 import requests
 from wit.wit import Wit
 
-import commands
 import config
 
 def message_received(msg):
-	split = msg.split(' ', 1)
-	if len(split) == 1:
-		return
-	command, text = split
-	handler = commands.handlers.get(command[1:])
-	if not handler:
-		return
-	try:
-		response = handler(text)
-	except:
-		response = traceback.format_exc()
-	if response:
-		respond(response)
+	print(msg)
 
 def respond(text):
 	response = text.replace('\n', '<br>')
 	channel_id = mumble.users.myself.get('channel_id')
 	mumble.channels[channel_id].send_text_message(response)
-
-class WikipediaParser(HTMLParser):
-	NOT_STARTED = 0
-	STARTED = 1
-	DONE = 2
-
-	def __init__(self):
-		HTMLParser.__init__(self)
-		self.state = self.NOT_STARTED
-		self.text = ''
-
-	def handle_starttag(self, tag, attrs):
-		if tag == 'p' and self.state == self.NOT_STARTED:
-			self.state = self.STARTED
-
-	def handle_endtag(self, tag):
-		if tag == 'p':
-			self.state = self.DONE
-
-	def handle_data(self, data):
-		if self.state == self.STARTED:
-			self.text += data
 
 def encode_mp3(pcm):
 	lame = subprocess.Popen(['lame', '--silent', '-f', '-V9', '-', '-'],
@@ -73,35 +38,21 @@ def encode_mp3(pcm):
 
 def query_wit(audio):
 	meaning = wit.post_speech(audio, content_type='mpeg3')
-	msg = meaning['msg_body']
-	entities = meaning['outcome']['entities']
-	wikipedia_search_query = entities.get('wikipedia_search_query')
-	if 'wikipedia' in msg.lower() and wikipedia_search_query:
-		query = urllib.urlencode({
-			'action': 'parse',
-			'page': wikipedia_search_query['value'],
-			'format': 'json',
-			'redirects': '1',
-		})
-		r = requests.get('https://en.wikipedia.org/w/api.php?' + query)
-		json = r.json()
-		if 'parse' in json:
-			text = json['parse']['text']['*']
-			parser = WikipediaParser()
-			parser.feed(text)
-			respond(parser.text)
-
-			index = parser.text.find('.')
-			espeak = subprocess.Popen(['espeak', '--stdout', '-v', 'en-us', parser.text[:index]],
-					stdout=subprocess.PIPE)
-			sox = subprocess.Popen(['sox', '-', '-r', '44100', '-t', 'raw', '-'],
-					stdin=espeak.stdout, stdout=subprocess.PIPE)
-			frames, _ = sox.communicate()
-			mumble.sound_output.add_sound(frames)
+	outcomes = meaning['outcomes']
+	for outcome in outcomes:
+		if outcome['intent'] == 'green_flame' and outcome['confidence'] >= 0.49:
+			break
+	else: # not green_flame
+		return
+	try:
+		wav = wave.open('green_flame.wav', 'r')
+		mumble.sound_output.add_sound(wav.readframes(wav.getnframes()))
+	finally:
+		wav.close()
 
 mumble = pymumble.Mumble(config.mumble_host, 64738, 'raylu-bot', config.mumble_password, debug=False)
 mumble.callbacks.set_callback(PYMUMBLE_CLBK_TEXTMESSAGERECEIVED, message_received)
-wit = Wit('EAW4ILIXTPSO57IIPLN7JYHMV2OUNXLQ')
+wit = Wit(config.wit_token)
 
 mumble.set_receive_sound(True)
 mumble.start()

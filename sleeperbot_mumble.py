fd872ece9131b2d319ff8d4232d5265d57fb2b14
raylu <lurayl@gmail.com>
2017-10-21 00:04:53 -0700

    get rid of wit.ai integration

--- sleeperbot_mumble.py
+++ sleeperbot_mumble.py
@@ -6,9 +6,6 @@ from os import path
 import sys
 sys.path.append(path.normpath(path.join(path.dirname(path.abspath(__file__)), 'pymumble')))
 
-from collections import defaultdict
-import cStringIO
-import subprocess
 import thread
 import time
 import traceback
@@ -16,7 +13,6 @@ import wave
 
 import pymumble
 from pymumble.constants import PYMUMBLE_CLBK_TEXTMESSAGERECEIVED
-from wit.wit import Wit
 
 import config
 
@@ -29,22 +25,6 @@ def respond(text):
 	channel_id = mumble.users.myself.get('channel_id')
 	mumble.channels[channel_id].send_text_message(response)
 
-def encode_mp3(pcm):
-	lame = subprocess.Popen(['lame', '--silent', '-f', '-V9', '-', '-'],
-			stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-	lame.stdin.write(pcm)
-	mp3, _ = lame.communicate()
-	return mp3
-
-def query_wit(audio):
-	meaning = wit.post_speech(audio, content_type='mpeg3')
-	print(meaning)
-	outcomes = meaning['outcomes']
-	for outcome in outcomes:
-		if outcome['intent'] == 'green_flame' and outcome['confidence'] >= 0.45:
-			green_flame()
-			break
-
 def green_flame():
 	try:
 		wav = wave.open('green_flame.wav', 'r')
@@ -54,45 +34,11 @@ def green_flame():
 
 mumble = pymumble.Mumble(config.mumble_host, 64738, 'raylu-bot', config.mumble_password, debug=False)
 mumble.callbacks.set_callback(PYMUMBLE_CLBK_TEXTMESSAGERECEIVED, message_received)
-wit = Wit(config.wit_token)
 
 mumble.set_receive_sound(True)
 mumble.start()
 mumble.is_ready()
 channel = mumble.channels.find_by_name(config.mumble_channel)
 channel.move_in()
-
-user_states = defaultdict(bool)
-user_bufs = {}
-user_wavs = {}
 while mumble.is_alive():
-	for user in mumble.users.values():
-		session = user['session']
-		if user.sound.is_sound():
-			pcm = user.sound.get_sound(0.01).pcm
-			if session not in user_bufs:
-				buf = cStringIO.StringIO()
-				wav = wave.open(buf, 'w')
-				wav.setparams((1, 2, 48000, 0, 'NONE', 'not compressed'))
-				user_bufs[session] = buf
-				user_wavs[session] = wav
-			user_wavs[session].writeframes(pcm)
-			user_states[session] = True
-		else:
-			if user_states[session]:
-				user_wavs[session].close()
-				buf = user_bufs[session]
-				try:
-					n_bytes = buf.tell()
-					if 25000 < n_bytes < 500000:
-						mp3 = encode_mp3(buf.getvalue())
-						thread.start_new_thread(query_wit, (mp3,))
-					else:
-						print('rejected', n_bytes)
-				except:
-					respond(traceback.format_exc())
-				buf.close()
-				del user_bufs[session]
-				del user_wavs[session]
-			user_states[session] = False
-	time.sleep(0.01)
+	time.sleep(1)
